c8782ece64b16390b4f16bcad9a1450151063aa7
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-07-18 00:15:42 +0200

    add validation to user model

--- Model/user.js
+++ Model/user.js
@@ -1,11 +1,63 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
-  email: { type: String, unique: true },
-  password: { type: String },
-  role: { type: Boolean },
+  first_name: {
+    type: String,
+    default: null,
+    validate: {
+      validator: (val) => {
+        return validator.isAlphanumeric(val);
+      },
+      message: "Invalid first name",
+    },
+  },
+  last_name: {
+    type: String,
+    default: null,
+    validate: {
+      validator: (val) => {
+        return validator.isAlphanumeric(val);
+      },
+      message: "Invalid last name",
+    },
+  },
+  email: {
+    type: String,
+    requird: true,
+    unique: true,
+    validate: {
+      validator: (val) => {
+        return validator.isEmail(val);
+      },
+      message: "Invalid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (val) => {
+        return validator.isStrongPassword(val);
+      },
+      message: "Weak password",
+    },
+  },
+  phone: {
+    type: String,
+    unique: true,
+    validate: {
+      validator: (val) => {
+        return validator.isMobilePhone(val);
+      },
+      message: "Invalid phone number",
+    },
+  },
+  admin: { type: Boolean },
+  user_id: {
+    type: Number,
+    unique: true
+  }
 });
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
