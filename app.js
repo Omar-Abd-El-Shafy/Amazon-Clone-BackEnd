43b80031e3b7cdaa37cf02cfd75a7d97f5866dff
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-17 15:16:37 +0200

    I removed unused index File

--- app.js
+++ app.js
@@ -21,22 +21,23 @@ app.use(express.json());
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
+
 // server listening
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-
-
 //route test for authentication
+// app.use(auth())
 app.get("/welcome", auth, (req, res) => {
   res.status(200).send("Welcome");
 });
-
 //...................
 
-app.post("/register", register);
 
+app.post("/register", register);
 app.post("/login", login);
+
+
 
 module.exports = app;
