91815423b5b82eae66791035d862845f4885523f
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-17 22:24:30 +0200

    I removed token part and added role part in schema

--- Model/user.js
+++ Model/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   last_name: { type: String, default: null },
   email: { type: String, unique: true },
   password: { type: String },
-  token: { type: String },
+  role: { type: Boolean },
 });
 
 module.exports = mongoose.model("user", userSchema);
