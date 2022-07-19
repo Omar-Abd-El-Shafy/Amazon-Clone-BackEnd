1bdf0e58f480445923fd7dd47efe5014e5a22937
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-19 13:55:13 +0200

    removed getting password

--- controllers/userController/getUserProfile.js
+++ controllers/userController/getUserProfile.js
@@ -7,8 +7,8 @@ exports.getUserProfile = async (req, res) => {
   const user_id = req.user_id;
   const userProfile = await User.findOne({ user_id });
   if (userProfile) {
-    const { first_name, last_name, email, password, phone } = userProfile;
-    res.status(200).json({ first_name, last_name, email, password, phone });
+    const { first_name, last_name, email, phone } = userProfile;
+    res.status(200).json({ first_name, last_name, email, phone });
   } else {
     res.status(400).send("User not found ");
   }
