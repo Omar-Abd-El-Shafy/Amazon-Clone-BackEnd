b49a88167866eb102557adb081363168cd4027f6
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-19 13:54:36 +0200

    added update controller

--- controllers/userController/updateProfile.js
+++ controllers/userController/updateProfile.js
@@ -1,2 +1,14 @@
+const User = require("../../Model/user");
+
 //TO DO
-exports.updateProfile = async (req, res) => {};
+
+exports.updateProfile = async (req, res, next) => {
+
+  await User.updateOne({ user_id: req.user_id }, { ...req.body }, { new: true })
+    .then(({ first_name, last_name, email, phone, password }) => {
+      res.status(200).send({ first_name, last_name, email, phone, password });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
