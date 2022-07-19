900dcadb83cda390d523e949698aa3f11fa467f1
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-19 12:47:31 +0200

    controller of get User done

--- controllers/userController/getUserProfile.js
+++ controllers/userController/getUserProfile.js
@@ -2,4 +2,14 @@ const User = require("../../Model/user");
 const jwt = require("jsonwebtoken");
 
 //TO DO
-exports.getUserProfile = async (req, res) => {};
+
+exports.getUserProfile = async (req, res) => {
+  const user_id = req.user_id;
+  const userProfile = await User.findOne({ user_id });
+  if (userProfile) {
+    const { first_name, last_name, email, password, phone } = userProfile;
+    res.status(200).json({ first_name, last_name, email, password, phone });
+  } else {
+    res.status(400).send("User not found ");
+  }
+};
