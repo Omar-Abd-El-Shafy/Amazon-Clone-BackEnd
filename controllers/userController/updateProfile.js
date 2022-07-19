4a7cc6feb65ab6cf74b9b03cdcd149a55899cfd9
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-07-19 15:41:09 +0200

    encrypt password in update profile

--- controllers/userController/updateProfile.js
+++ controllers/userController/updateProfile.js
@@ -1,14 +1,21 @@
 const User = require("../../Model/user");
-
+const bcrypt = require("bcryptjs");
 //TO DO
 
 exports.updateProfile = async (req, res, next) => {
-
-  await User.updateOne({ user_id: req.user_id }, { ...req.body }, { new: true })
+  if (password) {
+    const password = await bcrypt.hash(req.body.password, 10);
+  }
+  await User.findOneAndUpdate(
+    { user_id: req.user_id },
+    { ...req.body, password },
+    { new: true }
+  )
     .then(({ first_name, last_name, email, phone, password }) => {
       res.status(200).send({ first_name, last_name, email, phone, password });
     })
     .catch((err) => {
+      console.log(err);
       next(err);
     });
 };
