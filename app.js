66a2318d06aa3f58f9c02b4ab5d3ac0ed0465d1f
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-20 11:04:10 +0200

    deploying server on heroku

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-//env variables
+const cors = require("cors");
 require("dotenv").config();
-//connect database
 require("./DataSource/database").connect();
-//midllewares [will add logs]
 const error = require("./MiddleWare/error");
-//routes
 const userRoute = require("./Routes/userRoute");
 
 //server
 const express = require("express");
 const app = express();
 const { API_PORT } = process.env;
-const port = process.env.PORT || API_PORT;
-//to parse post req
+const port = process.env.PORT || 3333;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//routes
+
+app.use(cors());
 app.use("/user", userRoute);
 //error handler [MUST be the last middleware]
+app.use("/", (req, res) => {
+  res.send("<h1>Hello s7s</h1>");
+});
 app.use("/", error);
 
-// server listening
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
