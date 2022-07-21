71d93e29fc07e106ab8c02b5f6a3856781d2f950
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-07-22 01:29:22 +0200

    add comments explaining user validator

--- MiddleWare/validators/userInputVaildators.js
+++ MiddleWare/validators/userInputVaildators.js
@@ -1,12 +1,20 @@
 const { body, oneOf, validationResult } = require("express-validator");
 
+// this file contains the customized validators to use them in userValidator
+
+// common validation methods:
+// exists(): check the field is not undefiend
+// notEmpty():  check if a value is not empty; that is, a string with a length of 1 or bigger.
+// bail(): Stops running validations if any of the previous ones have failed.
+// withMessage(): provides error message if the validation didn't pass
+
 // implement validators
 const name = [
   body("name")
     .exists()
     .notEmpty()
     .withMessage("Name is required")
-    .bail()
+    .bail() //to stop validation if the previous condition didn't pass
     .trim() // remove spaces from start & end
     .isLength({ min: 3 })
     .withMessage("Name min length is 3")
@@ -15,7 +23,7 @@ const name = [
   // ignore: " ": ignore space in the middle to enter first and last name [optional]
   oneOf([
     body("name")
-      .isAlpha("en-US", { ignore: " " })
+      .isAlpha("en-US", { ignore: " " }) // check name contains letters only
       .withMessage("Name must contain letters only")
       .bail(),
     body("name")
@@ -48,6 +56,8 @@ const password = body("password")
   .notEmpty()
   .withMessage("Password is required")
   .bail()
+  // check password contains at least 1 lowercase letter,
+  // 1 uppercase letter, 1 symbol, and min length is 8
   .isStrongPassword()
   .withMessage("Weak password")
   .bail();
@@ -57,11 +67,13 @@ const confirmPassword = body("confirm_password")
   .notEmpty()
   .withMessage("Confirm password password is required")
   .bail()
+  // check confirm password matches password
   .custom((value, { req }) => value === req.body.password)
   .withMessage("Passwords did not match")
   .bail();
 
 // result middleware
+// Extracts the validation errors from a request and makes them available in a Result object.
 const validationResults = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty())
--- MiddleWare/validators/userValidator.js
+++ MiddleWare/validators/userValidator.js
@@ -9,6 +9,11 @@ const {
   validationResults,
 } = require("./userInputVaildators");
 
+// this is the validator used in userRoute
+
+// IMPORTANT
+// all validators must have validationResults as last item in the array
+
 const registerValidator = [
   name,
   oneOf([email, phone]),
@@ -17,20 +22,14 @@ const registerValidator = [
   validationResults,
 ];
 
-const loginValidator = [
-  oneOf([email, phone]),
-  password,
-  validationResults,
-]
+const loginValidator = [oneOf([email, phone]), password, validationResults];
 
+// in amazon, user can update only one field at a time, so we validate using oneOf
+// oneOf Creates a middleware instance that will ensure at least one of the given chains
+// passes the validation
 const updateValidator = [
-  oneOf([
-    name,
-    password,
-    email,
-    phone
-  ]),
-  validationResults
-]
+  oneOf([name, password, email, phone]),
+  validationResults,
+];
 
 exports.userValidator = { registerValidator, loginValidator, updateValidator };
