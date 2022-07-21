2af3db088678a5b870fdcb6227c7a7e91db3ccfe
Omar Abd El Shafy <omarabdelshafy1010@gmail.com>
2022-07-21 02:12:02 +0200

    added comments to modify schema

--- Model/product.js
+++ Model/product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
   product_id: { type: Number },
   name: { type: String, required: true },
-  description: { type: String },
+  description: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
   image_path: { type: String },
@@ -15,6 +15,25 @@ const productSchema = mongoose.Schema({
     category_id: { type: Number },
     category_name: { type: String },
   },
+  //ratings
+  //reviews
+  //COD true or false
+
+
+  //brand
+  //weight
+
+  //sorting:
+  //ratings// rating number
+  //price    //low to high //high to low
+
+  
+  //filter:
+  //department
+  //brand
+  //avilabilty
+
+  
 });
 
 module.exports = mongoose.model("Product", productSchema);
