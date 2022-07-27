9e220a7f212529c05e6f8c9ab95f6d527308de11
muhammadabdalla <muhammadabdalla51@gmail.com>
2022-07-27 15:52:10 +0200

    index merged from abdallah

--- controllers/userController/index.js
+++ controllers/userController/index.js
@@ -3,14 +3,11 @@ const { login } = require("./login");
 const { register } = require("./register");
 const { getUserProfile } = require("./getUserProfile");
 const { deleteAccount } = require("./deleteAccount");
-<<<<<<< HEAD
 const { forgotPassword } = require("./forgotPassword");
 const { resetPassword_get } = require("./resetPassword_get");
 const { resetPassword_post } = require("./resetPassword_post");
-=======
 const { updateProfile } = require("./updateProfile");
 const { updatePassword } = require("./updatePassword");
->>>>>>> d014429cb2ceee484a58330cb863e6d4c98eb3be
 
 //combine methods in userController obj
 const userController = {
@@ -19,13 +16,10 @@ const userController = {
   getUserProfile,
   updateProfile,
   deleteAccount,
-<<<<<<< HEAD
   forgotPassword,
   resetPassword_get,
   resetPassword_post,
-=======
-  updatePassword
->>>>>>> d014429cb2ceee484a58330cb863e6d4c98eb3be
+  updatePassword,
 };
 
 //to use in userRoute
