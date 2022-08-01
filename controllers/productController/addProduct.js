1ef18f1d3a5d8384fe3c790f556fc5527df23449
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-01 11:59:57 +0200

    throwing error if no req.files

--- controllers/productController/addProduct.js
+++ controllers/productController/addProduct.js
@@ -1,52 +1,51 @@
 const Product = require("../../Model/product");
 
 exports.addProduct = async (req, res, next) => {
-  try {
-    let image_path = req.files.map((file) => {
-      return file.location;
-    });
-     console.log(image_path);
-    // res.json({ status: "OK", uploaded: req.files.length });
-    const {
-      name,
-      description,
-      price,
-      quantity,
-      department,
-      category,
-      brand,
-      weight,
-      stock,
-    } = req.body;
-
-    let oldProduct;
-    if (name) {
-      oldProduct = await Product.findOne({ name });
-    }
-    if (oldProduct) {
-      return res.status(409).send("A Product already Exist with Same Name");
-    }
-    // Create Product in our database
-    await Product.create({
-      name,
-      description,
-      price, // convert email to lowercase
-      quantity,
-      image_path,
-      department,
-      category,
-      brand,
-      weight,
-      stock,
-    })
-      .then((product) => {
-        res.status(201).json(product);
-      })
-      .catch((err) => {
-        err.statusCode = 400;
-        next(err);
-      });
-  } catch (err) {
-    next(err);
+  if (!req.files) {
+    throw new Error("lol");
   }
+  let image_path = req.files.map((file) => {
+    return file.location;
+  });
+
+  console.log(image_path);
+  const {
+    name,
+    description,
+    price,
+    quantity,
+    department,
+    category,
+    brand,
+    weight,
+    stock,
+  } = req.body;
+
+  let oldProduct;
+  if (name) {
+    oldProduct = await Product.findOne({ name });
+  }
+  if (oldProduct) {
+    return res.status(409).send("A Product already Exist with Same Name");
+  }
+  // Create Product in our database
+  await Product.create({
+    name,
+    description,
+    price, // convert email to lowercase
+    quantity,
+    image_path,
+    department,
+    category,
+    brand,
+    weight,
+    stock,
+  })
+    .then((product) => {
+      res.status(201).json(product);
+    })
+    .catch((err) => {
+      err.statusCode = 400;
+      next(err);
+    });
 };
