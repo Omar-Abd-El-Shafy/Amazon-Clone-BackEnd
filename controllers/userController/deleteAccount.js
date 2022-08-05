941f06c478d1f39f5b2d9e2d05c61e68a7873c10
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-05 20:47:13 +0200

    deleting cart after then ,in delete account contro

--- controllers/userController/deleteAccount.js
+++ controllers/userController/deleteAccount.js
@@ -1,10 +1,14 @@
 const User = require("../../Model/user");
-
+const Cart = require("../../Model/cart");
 exports.deleteAccount = async (req, res, next) => {
   try {
-    const deletedUser = await User.deleteOne({ _id: req.user_id })
-      .then((data) => res.status(200).send(`User ${data.name} deleted.`))
-      .catch((err) => next(err));
+    const deletedUser = await User.findById(req.user_id);
+    await deletedUser.deleteOne();
+
+    await Cart.deleteOne({ user: req.user_id }).then(() => {
+      res.status(200).send(`User ${deletedUser.name} deleted.`);
+    });
+
     // await deletedUser
     //   .remove()
     //   .then((data) => res.status(200).send(`User ${data.name} deleted.`))
