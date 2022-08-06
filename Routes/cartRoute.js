cedd1f61923c757069e31c4d319dfc221502e4fa
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-06 19:30:01 +0200

    car routes

--- Routes/cartRoute.js
+++ Routes/cartRoute.js
@@ -1,16 +1,16 @@
 //route associated with "/product" in server
 const express = require("express");
-const productRoute = express.Router();
+const cartRoute = express.Router();
 const auth = require("../MiddleWare/auth");
 //controller
 
 const cartController = require("../controllers/cartController");
 
-productRoute.get("/getCart", auth, cartController.getCart);
-productRoute.put("/addItem", auth, cartController.addItem);
-productRoute.put("/removeItem", auth, cartController.removeItem);
-productRoute.put("/emptyCart", auth, cartController.emptyCart);
+cartRoute.get("/getCart", auth, cartController.getCart);
+cartRoute.put("/addItem", auth, cartController.addItem);
+cartRoute.put("/removeItem", auth, cartController.removeItem);
+cartRoute.put("/emptyCart", auth, cartController.emptyCart);
 
 //http methods
 
-module.exports = productRoute;
+module.exports = cartRoute;
