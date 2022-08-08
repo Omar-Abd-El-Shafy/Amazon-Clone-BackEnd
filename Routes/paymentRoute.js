541b22915d2eb0b081d491e9cd2d5ed3ba1dec7d
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-09 01:48:42 +0200

    express raw in route back with console hello

--- Routes/paymentRoute.js
+++ Routes/paymentRoute.js
@@ -7,6 +7,7 @@ const paymentController = require("../controllers/paymentController/index");
 paymentRoute.post("/create-payment-intent", auth, paymentController.payment);
 paymentRoute.post(
   "/webhook",
+  express.raw({ type: "application/json" }),
   paymentController.paymentCheck
 );
 module.exports = paymentRoute;
