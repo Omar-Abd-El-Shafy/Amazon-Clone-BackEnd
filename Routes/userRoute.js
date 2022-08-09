0bd5d03ca3e1d993031614e304fae7be666ec145
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-08-09 12:13:12 +0200

    add user route

---
+++ Routes/userRoute.js
@@ -0,0 +1,89 @@
+//route associated with "/user" in server
+const express = require("express");
+const userRoute = express.Router();
+//controller
+const userController = require("../controllers/userController");
+//midllewares
+const auth = require("../MiddleWare/auth");
+const isAdmin = require("../MiddleWare/adminAuth");
+const {
+  registerValidator,
+  forgotPasswordValidator,
+  resetPasswordValidator,
+  loginValidator,
+  updateUserValidator,
+  pageValidator,
+} = require("../MiddleWare/validators");
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The user _id
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *           format: email
+ *         password:
+ *           type: string
+ *           format: password
+ *         phone:
+ *           type: string
+ *       required:
+ *         - id
+ *         - name
+ *         - email
+ *         - password
+ *         - phone
+ */
+
+// route for register
+userRoute.post("/register", registerValidator, userController.register);
+
+userRoute.post(
+  "/forgotPassword",
+  forgotPasswordValidator,
+  userController.forgotPassword
+);
+
+userRoute.get(
+  "/password-reset/:id/:token",
+  auth,
+  userController.resetPasswordStart
+);
+
+userRoute.post(
+  "/password-reset/:id/:token",
+  auth,
+  resetPasswordValidator,
+  userController.resetPasswordSubmit
+);
+
+
+// route for login
+userRoute.post("/login", loginValidator, userController.login);
+
+// route for get user data
+userRoute.get("/", auth, userController.getUserProfile);
+
+// route for get all users data [only for admin]
+// page is passed in query params [?page=]
+userRoute.get("/allUsers", isAdmin, pageValidator, userController.getAllUsers);
+
+// route for delete user
+userRoute.delete("/", auth, userController.deleteAccount);
+
+// routes for update user profile
+// to update name or email or phone
+userRoute.put("/", auth, updateUserValidator, userController.updateProfile);
+
+userRoute.put("/password", auth, resetPasswordValidator, userController.updatePassword);
+
+
+module.exports = userRoute;
