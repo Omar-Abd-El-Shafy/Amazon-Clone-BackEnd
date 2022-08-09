1cb5b918e5a678ed044d8bbc8952565c651c9848
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-09 03:05:26 +0200

    fd

--- controllers/paymentController/paymentCheck.js
+++ controllers/paymentController/paymentCheck.js
@@ -11,11 +11,8 @@
 // 3) Run the server on http://localhost:4242
 //   node server.js
 
-const stripe = require("stripe")(
-  "sk_test_51LTllPFwhSEkFDCIq8x9nTSaTw616bbHe9Sg7KKIOO6HpWs4QshU2SdPqGWE3KL9vPw9fTbfOU4iDg9FeciXJIJo00yPwCTY5T"
-);
+const stripe = require("stripe");
 const express = require("express");
-const app = express();
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 const endpointSecret = "whsec_C1iyXwYMLycLTrCnBFaXhBAOM470Zylo";
@@ -26,12 +23,12 @@ exports.paymentCheck = (request, response) => {
 
   let event;
 
-  // try {
-  //   event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  // } catch (err) {
-  //   response.status(400).send(`Webhook Error: ${err.message}`);
-  //   return;
-  // }
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
   console.log("after erro msg");
   // Handle the event
   let paymentIntent = null;
