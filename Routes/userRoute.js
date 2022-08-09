6a972ad39bcc96122eba0cd661bce62a0ca1b37a
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-09 11:29:31 +0200

    fixing userRoute

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -15,7 +15,34 @@ const {
   pageValidator,
 } = require("../MiddleWare/validators");
 
-//http methods
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The user _id
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *           format: email
+ *         password:
+ *           type: string
+ *           format: password
+ *         phone:
+ *           type: string
+ *       required:
+ *         - id
+ *         - name
+ *         - email
+ *         - password
+ *         - phone
+ */
+
 
 // route for register
 userRoute.post("/register", registerValidator, userController.register);
@@ -26,20 +53,14 @@ userRoute.post(
   userController.forgotPassword
 );
 
-userRoute.get(
-  "/password-reset/:id/:token",
-  auth,
-  userController.resetPasswordStart
-);
+// userRoute.post(
+//   "/password-reset/:id/:token",
+//   auth,
+//   resetPasswordValidator,
+//   userController.resetPassword
+// );
 
-userRoute.post(
-  "/password-reset/:id/:token",
-  auth,
-  resetPasswordValidator,
-  userController.resetPasswordSubmit
-);
-
-userRoute.put("/", auth, resetPasswordValidator, userController.updatePassword);
+userRoute.put("/password", auth, resetPasswordValidator, userController.updatePassword);
 
 // route for login
 userRoute.post("/login", loginValidator, userController.login);
@@ -56,6 +77,6 @@ userRoute.delete("/", auth, userController.deleteAccount);
 
 // routes for update user profile
 // to update name or email or phone
-userRoute.put(["/"], auth, updateUserValidator, userController.updateProfile);
+userRoute.put("/", auth, updateUserValidator, userController.updateProfile);
 
 module.exports = userRoute;
