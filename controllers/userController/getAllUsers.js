02f84ef936347c64a8e65c11142f899e667dbb44
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-08-13 12:35:24 +0200

    send user data without password

--- controllers/userController/getAllUsers.js
+++ controllers/userController/getAllUsers.js
@@ -4,10 +4,9 @@ exports.getAllUsers = async (req, res, next) => {
   const itemsPerPage = req.query.itemsPerPage || 10,
     page = req.query.page - 1 || 0;
   await User.find({})
-    .select("name email phone")
+    .select("-password")
     .limit(itemsPerPage)
     .skip(page * itemsPerPage)
-    // .sort({ createdAt: "ascending" })
     .then((users) => res.status(200).json(users))
     .catch((err) => {
       next(err);
