2ff123a8069232940ddadf695bd369fbfbf28e97
Radwa Ayman <72885354+radwaymn@users.noreply.github.com>
2022-08-13 17:12:55 +0200

    enhance update profile

--- controllers/userController/updateProfile.js
+++ controllers/userController/updateProfile.js
@@ -4,14 +4,40 @@ const User = require("../../Model/user");
 // update password has its own method
 exports.updateProfile = async (req, res, next) => {
   try {
-    //  req.body contains name or email or phone
-    //  so we pass it dirctely to findOneAndUpdate as the update object
+    // Get user input
+    const { name, email, phone } = req.body;
+    let toUpdate = {};
+
+    // check if unique email or phone
+    // and append value to toUpdate obj
+    if (email) {
+      let oldUser = await User.findOne({ email });
+      if (oldUser) {
+        return res.status(409).send("Email already exists.");
+      }
+      toUpdate.email = email;
+    } else if (phone) {
+      let oldUser = await User.findOne({ phone });
+      if (oldUser) {
+        return res.status(409).send("Phone already exists.");
+      }
+      toUpdate.phone = phone;
+    } else if (name) {
+      toUpdate.name = name;
+    }
+
     // update in db
-    await User.findByIdAndUpdate(req.user_id, req.body, {
+    const user = await User.findByIdAndUpdate(req.user_id, toUpdate, {
       new: true,
-    }).then((updatedUser) => {
-      res.status(200).send("Profile updated");
     });
+
+    if (!user) {
+      res.status(404).send("User not found");
+    }
+
+    const sendUser = { ...user._doc };
+    delete sendUser.password;
+    res.status(200).send({ user: sendUser });
   } catch (err) {
     next(err);
   }
