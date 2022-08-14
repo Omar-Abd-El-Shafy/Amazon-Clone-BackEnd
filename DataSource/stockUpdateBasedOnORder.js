7259f0060870c84791ffdd922500900ed7fa0f02
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-14 23:15:53 +0200

    populate all

--- DataSource/stockUpdateBasedOnORder.js
+++ DataSource/stockUpdateBasedOnORder.js
@@ -5,20 +5,25 @@ exports.updateStock = async (transaction_id, status) => {
   try {
     const order = await Order.findOne({
       transaction_id: transaction_id,
-    }).populate("products.productBrief.product_id", "_id");
+    }).populate("products.productBrief.product_id");
 
     //if flag true then increase the stock to get back to original stock
+
     order.products.map(async (product) => {
       await Product.updateOne(
         { _id: product.productBrief.product_id._id },
         { $inc: { stock: product.quantity } }
+      );
+      console.log("producttttttt)($#)@_");
+      console.log(product);
+      console.log(
+        "_id of product to get updated-------====+++",
+        product.productBrief.product_id._id
       );
     });
     //flag is false remove the stock as order is completed
 
     order.status = status;
     await order.save();
-  } catch (err) {
-    next(err);
-  }
+
 };
