ff1801de1a0154f57f393e9b2d866546dbc7257a
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-14 23:27:37 +0200

    populate stock only

--- DataSource/stockUpdateBasedOnORder.js
+++ DataSource/stockUpdateBasedOnORder.js
@@ -5,7 +5,7 @@ exports.updateStock = async (transaction_id, status) => {
   try {
     const order = await Order.findOne({
       transaction_id: transaction_id,
-    }).populate("products.productBrief.product_id");
+    }).populate("products.productBrief.product_id", "stock");
 
     //if flag true then increase the stock to get back to original stock
 
@@ -14,12 +14,14 @@ exports.updateStock = async (transaction_id, status) => {
         { _id: product.productBrief.product_id._id },
         { $inc: { stock: product.quantity } }
       );
+
       console.log("producttttttt)($#)@_");
       console.log(product);
       console.log(
-        "_id of product to get updated-------====+++",
+        "_id of product to get updated-------====)())()()",
         product.productBrief.product_id._id
       );
+      console.log("quantity .... of order  --------_++", product.quantity);
     });
     //flag is false remove the stock as order is completed
 
