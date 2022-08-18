1cc282b1c382e112b93195ecc3df4b01191ff491
Omar-Abd-El-Shafy <omarabdelshafy1010@gmail.com>
2022-08-18 22:09:45 +0200

    send 200 i nin no product

--- controllers/cartController/getCart.js
+++ controllers/cartController/getCart.js
@@ -13,7 +13,7 @@ exports.getCart = async (req, res, next) => {
         res.status(200).send(cart);
       });
     } else {
-      res.status(204).send([]);
+      res.status(200).send([]);
     }
   } catch (err) {
     next(err);
